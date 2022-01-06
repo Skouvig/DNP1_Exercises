5da734a90d5f441d62e9785229b3b2f827650cfc
skouv <skouvig123@hotmail.com>
2022-01-06 20:47:53 +0100

    Finished Todo (JSON)

---
+++ BlazerApp/Data/ITodoData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BlazerApp.Models;
+
+namespace BlazerApp.Data
+{
+    public interface ITodoData
+    {
+        IList<Todo> GetTodos();
+        void AddTodo(Todo todo);
+        void RemoveTodo(int todoId);
+        void Update(Todo todo);
+        Todo Get(int id);
+    }
+}
